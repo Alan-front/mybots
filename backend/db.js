7779a4dd8f7c87bef183cb0035da3a8cf0e77c93
Alan-front <amiguelperez@proton.me>
2025-11-19 18:08:14 -0500

    edit db

--- backend/db.js
+++ backend/db.js
@@ -8,6 +8,7 @@ const pool = new Pool({
   connectionString:
     process.env.DATABASE_URL ||
     `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  ssl: { rejectUnauthorized: false },
 });
 
 pool.on("connect", () => {
