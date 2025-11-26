792d799317a0ee6b77b658e999202307103f98ae
Alan-front <amiguelperez@proton.me>
2025-11-25 23:14:28 -0500

    fix db config

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,14 @@
 import pkg from "pg";
 const { Pool } = pkg;
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const pool = new Pool({
-  connectionString:
-    process.env.DATABASE_URL ||
-    `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
-  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  ssl:
+    process.env.PGHOST !== "localhost" ? { rejectUnauthorized: false } : false,
 });
 
 pool.on("connect", () => {
@@ -18,11 +18,5 @@ pool.on("connect", () => {
 pool.on("error", (err) => {
   console.error("❌ Error en PostgreSQL:", err);
 });
-
-// test conexión
-pool
-  .query("SELECT NOW()")
-  .then((res) => console.log("✅ BD conectada:", res.rows[0]))
-  .catch((err) => console.error("❌ Error conectando a BD:", err));
 
 export default pool;
